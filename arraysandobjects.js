f4e979b9570ab9a4d85865e82fdb5bc6014d237d
Kushal Dotel <kushal.dotel@study.lbef.edu.np>
2024-09-15 17:27:17 +0545

    idk why I am active today

--- arraysandobjects.js
+++ arraysandobjects.js
@@ -1,9 +1,17 @@
 const ages = [1,2,3,4,5,6,7,8,9,10];
 
+biggest = 0;
+smallest = 10000000;
+
 for (let i=0; i<ages.length; i++){
-    if (ages[i] % 2 == 0){
-        console.log(ages[i]+ " is even.");
-    }else{
-        console.log(ages[i]+ " is odd.");
+    if (ages[i] > biggest){
+        biggest = ages[i];
+
+    }
+    if (ages[i] < smallest){
+        smallest = ages[i];
     }
 }
+
+console.log("The biggest number is " + biggest);
+console.log("The smallest number is " + smallest);
---
+++ maxminfunc.js
@@ -0,0 +1,5 @@
+const arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3];
+const max = Math.max(...arr);
+const min = Math.min(...arr);
+console.log(max); // Output: 9
+console.log(min)

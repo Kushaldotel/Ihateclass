5ab2a867f8e30a2757f0ad00ea7e25e38a777ff3
Kushal Dotel <kushal.dotel@study.lbef.edu.np>
2024-09-22 22:36:28 +0545

    promises recap

--- asynchronousnature.js
+++ asynchronousnature.js
@@ -7,3 +7,5 @@ fs.readFile("test.txt", "utf8", function(err,data){
 console.log("This is a message");
 
 // Promises are just a syntactical sugar for callbacks
+
+// Promises are objects that represent the eventual completion or failure of an asynchronous operation
